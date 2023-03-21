37e9253b7bfdf35f1dbea2bd99f3352e43df9c4f
Arsen Badalyan <arsenbadalyan.01@gmail.com>
2023-03-21 16:35:06 +0400

    feat: counter added, changes in Makefile, norminette

--- includes/game.h
+++ includes/game.h
@@ -1,80 +1,81 @@
 #ifndef GAME_H
-#define GAME_H
+# define GAME_H
 
 // General Defines
-#define GAME_NAME "so_long"
-#define GAME_SPEED 20
-#define CORRECT_C "10PECV"
-#define HAVE_ACCESS "0CE"
+# define GAME_NAME "so_long"
+# define GAME_SPEED 20
+# define CORRECT_C "10PECV"
+# define HAVE_ACCESS "0CE"
 
 // Window
-#define WIDTH  64
-#define HEIGHT 64
+# define WIDTH  64
+# define HEIGHT 64
 
 // Flags
-#define PLAYER_FLAG 'P'
-#define EXIT_FLAG   'E'
-#define ENEMY_FLAG  'V'
-#define WALL_FLAG   '1'
-#define SPACE_FLAG  '0'
-#define COIN_FLAG   'C'
-#define LOSE_FLAG   'L'
+# define PLAYER_FLAG 'P'
+# define EXIT_FLAG   'E'
+# define ENEMY_FLAG  'V'
+# define WALL_FLAG   '1'
+# define SPACE_FLAG  '0'
+# define COIN_FLAG   'C'
+# define LOSE_FLAG   'L'
 
 // KEY CODES
-#define LEFT_KEY 0
-#define BOTTOM_KEY 1
-#define RIGHT_KEY 2
-#define UP_KEY 13
-#define LEFT_ARROW 123
-#define BOTTOM_ARROW 125
-#define RIGHT_ARROW 124
-#define UP_ARROW 126
-#define ESC_KEY 53
+# define LEFT_KEY 0
+# define BOTTOM_KEY 1
+# define RIGHT_KEY 2
+# define UP_KEY 13
+# define LEFT_ARROW 123
+# define BOTTOM_ARROW 125
+# define RIGHT_ARROW 124
+# define UP_ARROW 126
+# define ESC_KEY 53
 
 // FLAG_PATHS
-#define PLAYER_PATH "./sprites/player/"
-#define ENEMY_PATH "./sprites/enemy/"
-#define EXIT_PATH "./sprites/exit/"
-#define COIN_PATH "./sprites/coin/"
-#define WALL_PATH "./sprites/wall/"
-#define LOSE_PATH "./sprites/effect_w.xpm"
+# define PLAYER_PATH "./sprites/player/"
+# define ENEMY_PATH "./sprites/enemy/"
+# define EXIT_PATH "./sprites/exit/"
+# define COIN_PATH "./sprites/coin/"
+# define WALL_PATH "./sprites/wall/"
+# define LOSE_PATH "./sprites/effect_w.xpm"
 
 // Max Animation Sprites
-#define MAX_ANIM_PLAYER 3
-#define MAX_ANIM_ENEMY  3
+# define MAX_ANIM_PLAYER 3
+# define MAX_ANIM_ENEMY  2
 
 typedef struct s_game {
-    struct s_mlx *mlx;
-    struct s_map *map;
-    struct s_status *status;
-    void **sprites;
-} t_game;
+	struct s_mlx	*mlx;
+	struct s_map	*map;
+	struct s_status	*status;
+	void			**sprites;
+}	t_game;
 
 typedef struct s_status {
-    size_t clc_coins;
-    size_t p;
-    size_t e;
-    size_t loop;
-    size_t loop_lose;
-    size_t  is_lose;
-} t_status;
+	size_t	clc_coins;
+	size_t	p;
+	size_t	e;
+	size_t	loop;
+	size_t	loop_lose;
+	size_t	is_lose;
+	size_t	move_counter;
+}	t_status;
 
 typedef struct s_mlx {
-    void *mlx;
-    void *win;
-} t_mlx;
+	void	*mlx;
+	void	*win;
+}	t_mlx;
 
 typedef struct s_map {
-    char    **map;
-    size_t  x;
-    size_t  y;
-    size_t  x_size;
-    size_t  y_size;
-    size_t  player[2];
-    size_t  coin;
-    size_t  enemy;
-    size_t  exit;
-    size_t  is_exit;
-} t_map;
+	char	**map;
+	size_t	x;
+	size_t	y;
+	size_t	x_size;
+	size_t	y_size;
+	size_t	player[2];
+	size_t	coin;
+	size_t	enemy;
+	size_t	exit;
+	size_t	is_exit;
+}	t_map;
 
 #endif
--- includes/so_long.h
+++ includes/so_long.h
@@ -1,75 +1,75 @@
 #ifndef SO_LONG_H
-#define SO_LONG_H
+# define SO_LONG_H
 
 // C Libs
-#include <stdio.h>
-#include <string.h>
-#include <fcntl.h>
-#include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <fcntl.h>
+# include <stdlib.h>
 
 // Custom Libraries
-#include <mlx.h>
-#include "game.h"
-#include "libft.h"
-#include "get_next_line.h"
+# include <mlx.h>
+# include "game.h"
+# include "libft.h"
+# include "get_next_line.h"
 
 // Error type defines
-#define WRONG_EXTENTION "wfe"
-#define WRONG_ARGUMENTS "wae"
-#define WRONG_MAP_CONF  "wmc"
-#define WRONG_WALL_CONF "wwc"
-#define WRONG_PE_CONF   "wpe"
-#define WRONG_WAY_CONF  "wwf"
+# define WRONG_EXTENTION "wfe"
+# define WRONG_ARGUMENTS "wae"
+# define WRONG_MAP_CONF  "wmc"
+# define WRONG_WALL_CONF "wwc"
+# define WRONG_PE_CONF   "wpe"
+# define WRONG_WAY_CONF  "wwf"
 
 // Functions
 
 // Initalization
-t_game *init_game();
-t_mlx *init_mlx();
-t_map *init_map();
-t_status *init_status();
+t_game		*init_game(void);
+t_mlx		*init_mlx(void);
+t_map		*init_map(void);
+t_status	*init_status(void);
 
 // Game
-void game_controller(t_game *game);
-void start_mlx(t_game *game);
-int exit_game(t_game *game);
+void		game_controller(t_game *game);
+int			exit_game(t_game *game);
+void		count_moves(t_game *game);
 
 // Draw functions
-int draw_map(t_game *game);
-void draw_lose(t_game *game, int w, int h);
-void *draw_sprite(t_game *game, char segment, int w, int h);
-char *draw_addn(t_game *game, char *fpath, int max_anim, size_t *status);
-void sprite_manipulation(t_game *game, char **map, int i);
+int			draw_map(t_game *game);
+void		draw_lose(t_game *game);
+void		*draw_sprite(t_game *game, char segment, int w, int h);
+char		*draw_addn(t_game *game, char *fpath, int max_anim, size_t *status);
+void		sprite_manipulation(t_game *game, char **map, int i);
 
 // Moves
-int pass_key(int key, t_game *game);
-void do_move(t_game *game, char **map, int x, int y);
-void player_lose(t_game *game, char **map, int x, int y);
+int			pass_key(int key, t_game *game);
+void		do_move(t_game *game, char **map, int x, int y);
+void		player_lose(t_game *game, char **map, int x, int y);
 
 // Validations
-void validate_map(char *file_name, t_game *game);
-void check_file_extention(char *file_name, t_game *game);
-char **map_check_controller(char *file_name, t_game *game, int fd);
-char *read_file_content(int fd);
-char **validate_file_content(char *file_content, t_game *game);
-void validate_map_size(char **map, t_game *game);
-int validate_walls(char **map);
-int check_wall(char *map_line, int is_centre);
-void fill_map_controller(char **map, t_game *game);
-int	find_characters(char **map, t_game *game, size_t *xy, char find);
-int find_game_paths(t_game *game);
-void fill_flood_map(t_map *map, size_t begin_x, size_t begin_y);
+void		validate_map(char *file_name, t_game *game);
+void		check_file_extention(char *file_name, t_game *game);
+char		**map_check_controller(t_game *game, int fd);
+char		*read_file_content(int fd);
+char		**validate_file_content(char *file_content, t_game *game);
+void		validate_map_size(char **map, t_game *game);
+int			validate_walls(char **map);
+int			check_wall(char *map_line, int is_centre);
+void		fill_map_controller(char **map, t_game *game);
+int			find_characters(char **map, t_game *game, size_t *xy, char find);
+int			find_game_paths(t_game *game);
+void		fill_flood_map(t_map *map, size_t begin_x, size_t begin_y);
 
 // Utils
-char **dup_double(char **str, size_t size);
+char		**dup_double(char **str, size_t size);
 
 // Error handle
-void catch_exception(int errno, char *addn);
-void handle_error(int exit_code, char *type);
+void		catch_exception(int errno, char *addn);
+void		handle_error(int exit_code, char *type);
 
 // Free functions
-char *free_single(char **addr);
-char *free_double(char ***addr);
-char *free_game(t_game **game);
+char		*free_single(char **addr);
+char		*free_double(char ***addr);
+char		*free_game(t_game **game);
 
 #endif
